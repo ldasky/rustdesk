02935a1d02d140663cfcc1facc806664be2e6226
ldasky <18188768@qq.com>
2024-09-05 12:37:08 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -894,7 +894,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://---".to_owned()
+    "http://nas.skltd.top:7000".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
